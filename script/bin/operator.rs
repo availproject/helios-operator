343d10783c4f6fc215d60406815f1212d38a74ea
Sasa Prsic <sasa@ethernal.tech>
2024-12-17 11:20:15 +0100

    Update groth16 proof.

--- script/bin/operator.rs
+++ script/bin/operator.rs
@@ -157,7 +157,7 @@ impl SP1AvailLightClientOperator {
         stdin.write_slice(&encoded_proof_inputs);
 
         // Generate proof.
-        let proof = self.client.prove(&self.pk, stdin).plonk().run()?;
+        let proof = self.client.prove(&self.pk, stdin).groth16().run()?;
 
         info!("Attempting to update to new head block: {:?}", latest_block);
         Ok(Some(proof))
