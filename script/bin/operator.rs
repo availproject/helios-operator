1acf4b0d4b8e4aa69ea672bb45a85511ae01f18e
Sasa Prsic <s.prsic@gmail.com>
2025-04-17 12:43:20 +0200

    fmt.

--- script/bin/operator.rs
+++ script/bin/operator.rs
@@ -23,8 +23,7 @@ use jsonrpsee::{
 use sp1_helios_primitives::types::ProofInputs;
 use sp1_helios_script::*;
 use sp1_sdk::{
-    NetworkProver, Prover, ProverClient, SP1ProofWithPublicValues, SP1ProvingKey,
-    SP1Stdin,
+    NetworkProver, Prover, ProverClient, SP1ProofWithPublicValues, SP1ProvingKey, SP1Stdin,
 };
 use std::env;
 use std::str::FromStr;
